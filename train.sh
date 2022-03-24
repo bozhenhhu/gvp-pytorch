f3da8717c45feef8fc40b52ba1b449417ce81e98
bozhenhhu <hubozhen@westlake.edu.cn>
2022-03-24 10:31:18 +0000

    hist

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python run_cpd.py --train\
     --wandb\
-    --experiment_name "train_sprof" --wandb_project GVP --wandb_entity bozhen\
+    --experiment_name "train_chi_square" --wandb_project GVP --wandb_entity bozhen\
     --v 30. --epochs 100 
